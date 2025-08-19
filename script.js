75e0043d873ab7fb6af4522cb8763088f31638be
Moses Rametsi <158572657+bblack-jewENT@users.noreply.github.com>
2025-08-19 23:54:30 +0200

    Update script.js
    Create a variable and add a new array.

---
+++ script.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".multiple-text", {
+    strings: ["Front-end Developer", "Videographer", "Photographer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
